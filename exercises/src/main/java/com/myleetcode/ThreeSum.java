30bfad6a132ead1d992e72f8b5b84dff02fe2b47
root <root@hash.us-west4-b.c.valid-dragon-326216.internal>
2021-09-21 17:32:30 +0000

    More implementation for ThreeSum

--- exercises/src/main/java/com/myleetcode/ThreeSum.java
+++ exercises/src/main/java/com/myleetcode/ThreeSum.java
@@ -1,8 +1,35 @@
 package com.myleetcode;
 
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * <h1>ThreeSum</h1>
+ * This class implements ThreeSum solution
+ * <p> Three Sum qn can be found in https://leetcode.com/problems/3sum/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2021-09-21
+ */
+
+
 public class ThreeSum
 {
 
+    public List<List<Integer>> threeSum(int[] nums)
+    {
+       List<List<Integer>> result;
 
+       if(nums == null || nums.length < 3)
+       {
+          return new ArrayList<>();
+       }
+
+       result = new ArrayList<>();
+
+       return result;
+    }
 
 }
