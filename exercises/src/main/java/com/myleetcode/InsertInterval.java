73972487908c24a492ff85cad8243926f2417fc4
satsrini <satsrinister@gmail.com>
2021-12-04 06:48:35 +0000

    Skeleton implementation of InsertInterval

---
+++ exercises/src/main/java/com/myleetcode/InsertInterval.java
@@ -0,0 +1,30 @@
+package com.myleetcode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * <h1>InsertInterval</h1>
+ * This class implements InsertInterval solution
+ * <p> InsertInterval qn can be found in https://leetcode.com/problems/insert-interval/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2021-12-04
+ */
+
+
+public class InsertInterval
+{
+
+    public int[][] insert(int[][] intervals, int[] newInterval)
+    {
+
+      List<Integer[]> intervalsList = new ArrayList<>();
+
+      return null;
+    }
+
+
+}
