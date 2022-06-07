4d14437c57b778a6bfcbe6445add2e29f0551ab6
root <root@localhost.localdomain>
2022-06-07 07:53:09 -0700

    Initial code upload for median of two equal lengthed sorted Integer Arrays

---
+++ exercises/src/main/java/com/myleetcode/MedianEqualArrays.java
@@ -0,0 +1,28 @@
+package com.myleetcode;
+
+
+/**
+ * <h1>MedianEqualArrays</h1>
+ * This class finds the median of two sorted integer arrays of equal length
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-06-07
+ */
+
+public class MedianEqualArrays
+{
+
+    public MedianEqualArrays()
+    {
+
+    }
+
+    public int solution(int[] a, int[] b)
+    {
+       int result = 0;
+
+       return result;
+    }
+
+}
