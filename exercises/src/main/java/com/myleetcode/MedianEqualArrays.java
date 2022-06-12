c90a4da8fb2434ecf3a13003d9ac4e7652fe18af
root <root@localhost.localdomain>
2022-06-12 01:35:58 -0700

    Midway

--- exercises/src/main/java/com/myleetcode/MedianEqualArrays.java
+++ exercises/src/main/java/com/myleetcode/MedianEqualArrays.java
@@ -26,26 +26,33 @@ public class MedianEqualArrays
     public double solution(int[] a, int[] b)
     {
 
-       int n = a.length;
 
        if(a.length == 1)
        {
           return ((double)(a[0] + b[0]))/2;
        }
 
+       int n = a.length;
+
        if(a[n-1] <= b[0])   //{a[0],a[n-1], b[0], b[n-1] }
        {
+          System.out.println("IN 1");
           return ((double)(a[n-1] + b[0]))/2;
        }
 
        if(a[0] >= b[n-1]) //{b[0],b[n-1], a[0], a[n-1] }
        {
+          System.out.println("IN 2");
+
           return ((double)(a[0] + b[n-1]))/2;
        }
 
        // if middle values are equal, then it is straight forward to calculate median
-       if(a[n/2] == b[n/2])
+       int midIndex = (n+1)/2-1; 
+       if(a[midIndex] == b[midIndex])
        {
+          System.out.println("IN 3");
+
           if(n % 2 == 1) // length is odd
           {
              return (double)(a[n/2]);
@@ -66,11 +73,16 @@ public class MedianEqualArrays
 
     private double median(int[] a, int ai, int aj, int[] b, int bi, int bj)
     {
+
+        System.out.println("In Median: " + ai + " " + aj + " " + bi + " " + bj);
         
         int aDiff = aj-ai;
 
         if(aDiff == 1) // coming to the end, time to calculate the combined median
         {
+
+            System.out.println("Diff is 1: " + ai + " " + aj + " " + bi + " " + bj);
+
             if(a[aj] <= b[bi]) // {a[ai],a[aj],b[bi],b[bj]}
             {
                 return ((double)(a[aj]+b[bi]))/2;
@@ -112,7 +124,7 @@ public class MedianEqualArrays
        System.out.println("8 is the answer: " + medianEqualArrays.solution(a,b));
     }*/
 
-    public static void main(String[] args)
+    /*public static void main(String[] args)
     {
        int[] a = { 5,8,10,11,14,15,17 };
        int[] b = { -17,-15,-12,0,1,2,2 };
@@ -120,6 +132,17 @@ public class MedianEqualArrays
        MedianEqualArrays medianEqualArrays = new MedianEqualArrays();
 
        System.out.println("3.5 is the answer: " + medianEqualArrays.solution(a,b));
+    }*/
+
+    public static void main(String[] args)
+    {
+
+       int[] a = {1,4,7,8 };
+       int[] b = { 5,6,7,9 };
+
+       MedianEqualArrays medianEqualArrays = new MedianEqualArrays();
+
+       System.out.println("6.5 is the answer: " + medianEqualArrays.solution(a,b));
     }
 
    /*public static void main(String[] args)
