4d976a37e789d15f2fcf5e5698365f4d5a3c04de
root <root@localhost.localdomain>
2022-06-15 02:24:59 -0700

    Success, atlast ?

--- exercises/src/main/java/com/myleetcode/ThreeSumAgain.java
+++ exercises/src/main/java/com/myleetcode/ThreeSumAgain.java
@@ -39,11 +39,28 @@ public class ThreeSumAgain
 
       for(;nums[i] <= 0 && i < nums.length-2;i++)
       {
+
+         if(i > 0 && nums[i] == nums[i-1]) // if the current element is same as previous one,then we can move onto next
+         {
+            continue;
+         }
          j = i+1;
          k = nums.length-1;
 
          while(j < k)
          {
+           if(j > i+1 && nums[j] == nums[j-1]) // if the current element is same as previous one,then we can move onto next
+           {
+              j++;
+              continue;
+           }
+
+           if(k < nums.length-1 && nums[k] == nums[k+1]) // if the current element is same as previous one,then we can move onto next
+           {
+              k--;
+              continue;
+           }
+
            if(nums[i] + nums[j] + nums[k] == 0)
            {
                List<Integer> internalList = new ArrayList<>();
