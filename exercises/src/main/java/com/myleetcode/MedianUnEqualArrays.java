e3074d7986f508250643c9542c35893247c1a84e
root <root@localhost.localdomain>
2022-06-18 22:23:05 -0700

    Started - median of unequal sorted arrays...

---
+++ exercises/src/main/java/com/myleetcode/MedianUnEqualArrays.java
@@ -0,0 +1,31 @@
+package com.myleetcode;
+
+
+/**
+ * <h1>MedianUnEqualArrays</h1>
+ * This class finds the median of two sorted integer arrays of unequal length.
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-06-19
+ */
+
+public class MedianUnEqualArrays
+{
+
+    public MedianUnEqualArrays()
+    {
+
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        return 0.0;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Unequal arrays");
+    }
+
+}
