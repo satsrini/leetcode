add2e0c1b36ef0c1429aba28f8eb47ffb0345298
root <root@localhost.localdomain>
2022-06-25 07:15:50 -0700

    Coding over, next testing

--- exercises/src/main/java/com/myleetcode/TwoDimensionalArray.java
+++ exercises/src/main/java/com/myleetcode/TwoDimensionalArray.java
@@ -4,7 +4,7 @@ package com.myleetcode;
 /**
  * <h1>TwoDimensionalArray</h1>
  * This class finds the number of possible paths from the given source coordinate to the given destination coordinate
- * in a recursive manner for the given two dimensinal array. It is assumed that the traversals can be done only in 
+ * in a recursive manner in a two dimensinal array. It is assumed that the traversals can be done only in 
  * 'right' and 'down' directions.
  * Actual problem statement:   "Count the number of ways to reach from a source to destination in a 2D integer array if * you can go in 2 directions - right and down."
  *
@@ -22,26 +22,42 @@ public class TwoDimensionalArray
     }
 
     /**
-      * a - given 2D array
       * sx,sy - source coordinates
       * dx,dy - destination coordinates
       **/
 
-    public int solution(int[][] a, int sx, int sy, int dx, int dy)
+    public int solution(int sx, int sy, int dx, int dy)
     {
 
-       if(a == null || a.length == 0 || sx > dx || sy < dy || (sx == dx && sy == dy))
+       if(sx > dx || sy < dy || (sx == dx && sy == dy))
        {
            return 0;
        }
 
-       return paths();
+       return paths(sx,sy,dx,dy);
 
     }
 
-    private int paths()
+    private int paths(int sx, int sy, int dx, int dy)
     {
-       return 0;
+       int count = 0;
+
+       if(sx == dx && sy == dy)
+       {
+          return 0;
+       }
+
+       if(sx < dx)
+       {
+           count = count + 1 + paths(sx+1,sy,dx,dy);
+       }
+ 
+       if(sy > dy)
+       {
+           count = count + 1 + paths(sx,sy-1,dx,dy);
+       }
+
+       return count;
     }
 
     public static void main(String[] args)
