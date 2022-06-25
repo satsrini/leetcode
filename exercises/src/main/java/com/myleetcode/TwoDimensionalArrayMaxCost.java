1debf10d0a07bab39a413fd74fba08babb41d72a
root <root@localhost.localdomain>
2022-06-25 08:40:48 -0700

    MaxCost - initial code

---
+++ exercises/src/main/java/com/myleetcode/TwoDimensionalArrayMaxCost.java
@@ -0,0 +1,28 @@
+package com.myleetcode;
+
+
+/**
+ * <h1>TwoDimensionalArrayMaxCost</h1>
+ * This class finds the max cost incurred when moving from the given source coordinate to the given destination 
+ * coordinate in a recursive manner in a two dimensinal array. It is assumed that the traversals can be done only in
+ * 'right' and 'down' directions.
+ * Every node has cost value 1 or 2 or 3 with the following rules
+ * cost 1 - can go right
+ * cost 2 - can go down
+ * cost 3 - can go both right and down
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-06-25
+ */
+
+public class TwoDimensionalArrayMaxCost
+{
+
+
+   public static void main(String[] args)
+   {
+       System.out.println("Hello TwoDimensionalArrayMaxCost");
+   }
+
+}
