027c0d5303393682c785ae9366cba3cee6c79f55
Satish Srinivasan <satsrinister@gmail.com>
2022-07-02 03:55:47 -0700

    implementation over, testing remaining

--- exercises/src/main/java/com/myleetcode/LongestPalindrome.java
+++ exercises/src/main/java/com/myleetcode/LongestPalindrome.java
@@ -31,11 +31,53 @@ public class LongestPalindrome
         int start = 0;
         int end = 0;
 
+        for(int i = 0; i < s.length(); i++)
+        {
+           int length1 = getMaxPalindromeLength(s,i,i);
+           int length2 = getMaxPalindromeLength(s,i,i+1);
 
+           int length = Math.max(length1,length2);
+
+           if(length > (end-start+1))
+           {
+              if(length % 2 == 1)
+              {
+                 start = i - length/2;
+              }else
+              {
+                 start = i - length/2 + 1;
+              }
+ 
+              end = i + length/2;
+              
+           }
+
+        }
 
 
   
        return s.substring(start,end+1);
+
+    }
+
+    private int getMaxPalindromeLength(String s, int a, int b)
+    {
+
+        if(s.charAt(a) != s.charAt(b))
+        {
+           return 1;
+        }
+
+        int l = a;
+        int r = b;
+
+        while(l > 0 && r < s.length() && s.charAt(l) == s.charAt(r))
+        {
+           l--;
+           r++;
+        }       
+
+        return r-l+1;
 
     }
 
