730f44dad47237c81b6aeb44060d417460821769
Satish Srinivasan <satsrinister@gmail.com>
2022-07-02 23:51:26 -0700

    initial class for Zigzag Conversion

---
+++ exercises/src/main/java/com/myleetcode/ZigzagConversion.java
@@ -0,0 +1,28 @@
+package com.myleetcode;
+
+
+/**
+ * <h1>ZigzagConversion</h1>
+ * This class gets the zigzag conversion of a given String
+ * <p> Zigzag Conversion qn can be found in https://leetcode.com/problems/zigzag-conversion/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-02
+ */
+
+public class ZigzagConversion
+{
+
+    public ZigzagConversion()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+       System.out.println("Hello Zigzag conversion");
+    }
+
+}
