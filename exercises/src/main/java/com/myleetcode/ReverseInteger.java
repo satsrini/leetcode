f8a6a45ae9ba55d76eade8604aeca0aaa9a08a5d
Satish Srinivasan <satsrinister@gmail.com>
2022-07-04 07:43:10 -0700

    Initial commit

---
+++ exercises/src/main/java/com/myleetcode/ReverseInteger.java
@@ -0,0 +1,41 @@
+package com.myleetcode;
+
+/**
+ * <h1>ReverseInteger.java</h1>
+ * This class reverses the given Integer
+ * <p> Reverse Integer qn can be found in https://leetcode.com/problems/reverse-integer/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-04
+ */
+
+public class ReverseInteger
+{
+ 
+   public ReverseInteger()
+   {
+
+   }
+
+   public int reverse(int x)
+   {
+      if(x == 0)
+      {
+         return 0;
+      }
+
+      int result = 0;
+
+
+      return result;
+   }
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello Reverse Integer");
+   }
+
+
+}
