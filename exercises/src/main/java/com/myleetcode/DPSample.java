ad51d0fd10f35ce8a9c83cfeba63bcfc074110e9
Satish Srinivasan <satsrinister@gmail.com>
2022-07-12 09:01:43 -0700

    Initial commit

---
+++ exercises/src/main/java/com/myleetcode/DPSample.java
@@ -0,0 +1,32 @@
+package com.myleetcode;
+
+/**
+ * <h1>DPSample.java</h1>
+ * Using Dynamic Programming, this class finds no of different possible ways of finding the sum N using three given 
+ * integers 1,3 and 4
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-12
+ */
+public class DPSample
+{
+
+   public DPSample()
+   {
+
+   }
+
+   public int solution(int N)
+   {
+
+       return 0;
+   }
+
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello DPSample");
+   }
+
+}
