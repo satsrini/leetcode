d926e83274e288badb9e52a06f762c4a8714de58
root <root@localhost.localdomain>
2022-07-23 06:57:35 -0700

    starting ...

---
+++ exercises/src/main/java/com/myleetcode/PascalsTriangle.java
@@ -0,0 +1,26 @@
+package com.myleetcode;
+
+/**
+ * <h1>PascalsTriangle.java</h1>
+ * This class implements PascalsTriangle solution using Dynamic Programming.
+ * <p> PascalsTriangle qn can be found in https://leetcode.com/problems/pascals-triangle/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-23
+ */
+public class PascalsTriangle
+{
+
+  public PascalsTriangle()
+  {
+
+  }
+
+  public static void main(String[] args)
+  {
+     System.out.println("Hello Pascals Triangle");
+  }
+
+}
