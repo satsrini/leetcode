69c581f272157325ed0d68f7ee67a04d32d6af10
root <root@localhost.localdomain>
2022-07-22 22:35:06 -0700

    ClimbingStairs done and wqdusted

---
+++ exercises/src/main/java/com/myleetcode/ClimbingStairs.java
@@ -0,0 +1,66 @@
+package com.myleetcode;
+
+/**
+ * <h1>ClimbingStairs.java</h1>
+ * This class implements ClimbingStairs solution using Dynamic Programming
+ * <p> ClimbingStairs qn can be found in https://leetcode.com/problems/climbing-stairs/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-23
+ */
+
+public class ClimbingStairs
+{
+
+  public ClimbingStairs()
+  {
+
+
+  }
+
+  public int climbStairs(int n)
+  {
+     if(n <= 0)
+     {
+        return 0;
+     }
+
+     if(n == 1)
+     {
+        return 1;
+     }
+
+     if(n == 2)
+     {
+        return 2;
+     }
+
+     int[] DP = new int[n+1];
+     DP[1] = 1;
+     DP[2] = 2;
+
+     // DP[n] = DP[n-1] 
+     for(int i = 3; i <= n; i++)
+     {
+        DP[i] = DP[i-1] + DP[i-2];
+     }
+
+     return DP[n];
+
+  }
+
+  public static void main(String[] args)
+  {
+     System.out.println("Hello Climbing Stairs");
+
+     int n = 5;
+
+     ClimbingStairs climbingStairs = new ClimbingStairs();
+
+     System.out.println("Should be 8:  " + climbingStairs.climbStairs(5));
+
+  }
+
+}
