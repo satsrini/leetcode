2e695f36c4a313d27fc6c1596bbd0102a66e175e
root <root@localhost.localdomain>
2022-07-22 22:53:36 -0700

    Comments enhancement

--- exercises/src/main/java/com/myleetcode/ClimbingStairs.java
+++ exercises/src/main/java/com/myleetcode/ClimbingStairs.java
@@ -42,7 +42,7 @@ public class ClimbingStairs
      DP[1] = 1;
      DP[2] = 2;
 
-     // DP[n] = DP[n-1] 
+     // DP[n] = DP[n-1] + DP[n-2]
      for(int i = 3; i <= n; i++)
      {
         DP[i] = DP[i-1] + DP[i-2];
