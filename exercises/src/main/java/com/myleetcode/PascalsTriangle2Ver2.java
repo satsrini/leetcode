9aa1a2d0e94791e3876ba400b516192b5b19383f
root <root@localhost.localdomain>
2022-07-27 06:44:05 -0700

    Initial commit

---
+++ exercises/src/main/java/com/myleetcode/PascalsTriangle2Ver2.java
@@ -0,0 +1,26 @@
+package com.myleetcode;
+
+/**
+ * <h1>PascalsTriangle2Ver2.java</h1>
+ * This class implements PascalsTriangle2Ver2 solution.
+ * <p> PascalsTriangle2 qn can be found in https://leetcode.com/problems/pascals-triangle-ii/
+ * This fixes the follow up goal "Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?"
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-24
+ */
+public class PascalsTriangle2Ver2
+{
+
+  public PascalsTriangle2Ver2()
+  {
+
+  }
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello PascalsTriangle2Ver2");
+  }
+}
