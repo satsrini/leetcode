705379da6ce803388229e3412ba5c4d752996c81
root <root@localhost.localdomain>
2022-07-28 22:32:45 -0700

    Triangle started ...

---
+++ exercises/src/main/java/com/myleetcode/Triangle.java
@@ -0,0 +1,28 @@
+package com.myleetcode;
+
+/**
+ * <h1>Triangle.java</h1>
+ * This class implements Triangle solution using Dynamic Programming.
+ * <p> Triangle qn can be found in https://leetcode.com/problems/triangle/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-29
+ */
+
+public class Triangle
+{
+
+  public Triangle()
+  {
+
+  }
+
+  public static void main(String[] args)
+  {
+     System.out.println("Hello Triangle");
+  }
+
+
+}
