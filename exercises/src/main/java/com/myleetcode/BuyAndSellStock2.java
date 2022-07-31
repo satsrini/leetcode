707b9b7c8d3738b5a0385d7780e9873115ab4ec0
Satish Srinivasan <satsrinister@gmail.com>
2022-07-31 08:27:26 -0700

    init

---
+++ exercises/src/main/java/com/myleetcode/BuyAndSellStock2.java
@@ -0,0 +1,26 @@
+package com.myleetcode;
+
+
+/**
+ * <h1>BuyAndSellStock2.java</h1>
+ * This class implements BuyAndSellStock2 solution.
+ * <p> BuyAndSellStock2 qn can be found in https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-07-31
+ */
+public class BuyAndSellStock2
+{
+
+   public BuyAndSellStock2()
+   {
+
+   }
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello BuyAndSellStock2");
+   }
+}
