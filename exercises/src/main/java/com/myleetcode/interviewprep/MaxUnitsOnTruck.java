9c3767b872360a04dcb8e5062a906abf8865b2d8
Satish Srinivasan <satsrinister@gmail.com>
2022-08-10 08:50:43 -0700

    Initial commit

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/MaxUnitsOnTruck.java
@@ -0,0 +1,24 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>MaxUnitsOnTruck.java</h1>
+ * This class implements MaxUnitsOnTruck solution.
+ * <p> MaxUnitsOnTruck qn can be found in https://leetcode.com/problems/maximum-units-on-a-truck/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-10
+ */
+public class MaxUnitsOnTruck
+{
+   public MaxUnitsOnTruck(){}
+
+   
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello MaxUnitsOnTruck");
+   }
+
+}
