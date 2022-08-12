67549ea6cce71404fa61f085f2abc359d654ee92
Satish Srinivasan <satsrinister@gmail.com>
2022-08-12 04:42:44 -0700

    Done in a flash today ?

--- exercises/src/main/java/com/myleetcode/interviewprep/MaxUnitsOnTruck.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/MaxUnitsOnTruck.java
@@ -1,5 +1,7 @@
 package com.myleetcode.interviewprep;
 
+import java.util.Arrays;
+
 /**
  * <h1>MaxUnitsOnTruck.java</h1>
  * This class implements MaxUnitsOnTruck solution.
@@ -14,11 +16,37 @@ public class MaxUnitsOnTruck
 {
    public MaxUnitsOnTruck(){}
 
-   
+   public int maximumUnits(int[][] boxTypes, int truckSize) 
+   {
+       int max = 0;
+
+       Arrays.sort(boxTypes, (a, b) -> (b[1]-a[1]));
+
+       for(int i = 0; i < boxTypes.length; i++)
+       {
+          max += Math.min(truckSize, boxTypes[i][0])*boxTypes[i][1];
+          truckSize -= boxTypes[i][0];
+
+          if(truckSize <= 0)
+          {
+             break;
+          }
+
+       }
+
+       return max;
+   }  
 
    public static void main(String[] args)
    {
       System.out.println("Hello MaxUnitsOnTruck");
+
+      int[][] boxTypes = {{1,3},{2,2},{3,1}};
+
+      MaxUnitsOnTruck maxUnitsOnTruck = new MaxUnitsOnTruck();
+
+      System.out.println("Should be 8:   " + maxUnitsOnTruck.maximumUnits(boxTypes,4));
+
    }
 
 }
