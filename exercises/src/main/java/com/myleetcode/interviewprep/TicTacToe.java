04ca85bf838dd55db48397e30b9e5ede943d8f9e
Satish Srinivasan <satsrinister@gmail.com>
2022-08-12 09:22:53 -0700

    Started

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/TicTacToe.java
@@ -0,0 +1,34 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>TicTacToe.java</h1>
+ * This class implements TicTacToe solution.
+ * <p> TicTacToe qn can be found in https://leetcode.com/problems/design-tic-tac-toe/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-12
+ */
+
+
+public class TicTacToe
+{
+
+  public TicTacToe(int n)
+  {
+
+  }
+
+  public int move(int row, int col, int player)
+  {
+
+    return 0;
+  }
+
+  public static void main(String[] args)
+  {
+     System.out.println("Hello TicTacToe");
+  }
+
+}
