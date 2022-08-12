8840317de3a2f8f7501dee8f326a5ab9bae40acf
Satish Srinivasan <satsrinister@gmail.com>
2022-08-12 09:55:14 -0700

    All done

--- exercises/src/main/java/com/myleetcode/interviewprep/TicTacToe.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/TicTacToe.java
@@ -15,13 +15,90 @@ package com.myleetcode.interviewprep;
 public class TicTacToe
 {
 
+  private int[][] board;
+  private int n;
+
   public TicTacToe(int n)
   {
-
+      this.n = n;
+      board = new int[n][n];
   }
 
   public int move(int row, int col, int player)
   {
+
+    board[row][col] = player;
+
+    boolean win = true;
+    // search the row
+    for(int i = 0; i < n; i++)
+    {
+       if(board[row][i] != player)
+       {
+          win = false;
+          break;
+       }
+    }
+
+    if(win)
+    {
+       return 1;
+    }
+
+    // search the column
+    win = true;
+    for(int i = 0; i < n; i++)
+    {
+       if(board[i][col] != player)
+       {
+          win = false;
+          break;
+       }
+    }
+
+    if(win)
+    {
+      return 1;
+    }
+
+    // diagonal down
+    if(row == col)
+    {
+       win = true;
+       for(int i = 0; i < n; i++)
+       {
+         if(board[i][i] != player)
+         {
+            win = false;
+            break;
+         }
+       }
+    }
+
+    if(win)
+    {
+       return 1;
+    }
+
+    // diagonal up
+    if(row == n-1-col)
+    {
+       win = true;
+       for(int i = 0; i < n; i++)
+       {
+          if(board[i][n-1-i] != player)
+          {
+             win = false;
+             break;
+          }
+       }
+
+    }
+
+    if(win)
+    {
+       return 1;
+    }
 
     return 0;
   }
@@ -29,6 +106,17 @@ public class TicTacToe
   public static void main(String[] args)
   {
      System.out.println("Hello TicTacToe");
+
+     TicTacToe ticTacToe = new TicTacToe(3);
+
+     System.out.println("Shoould be 0:   " + ticTacToe.move(0,0,1));
+     System.out.println("Shoould be 0:   " + ticTacToe.move(0,2,2));
+     System.out.println("Shoould be 0:   " + ticTacToe.move(2,2,1));
+     System.out.println("Shoould be 0:   " + ticTacToe.move(1,1,2));
+     System.out.println("Shoould be 0:   " + ticTacToe.move(2,0,1));
+     System.out.println("Shoould be 0:   " + ticTacToe.move(1,0,2));
+     System.out.println("Shoould be 1, wins:   " + ticTacToe.move(2,1,1));
+
   }
 
 }
