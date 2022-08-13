3eac4c09c07f2e746098787bf96b7db22cb989bf
Satish Srinivasan <satsrinister@gmail.com>
2022-08-13 09:34:23 -0700

    All done really ?

--- exercises/src/main/java/com/myleetcode/interviewprep/ValidTicTacToe.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/ValidTicTacToe.java
@@ -17,16 +17,64 @@ public class ValidTicTacToe
 
    public ValidTicTacToe(){}
 
-   public boolean validTicTacToe()
+   public boolean validTicTacToe(String[] board)
    {
+
+      StringBuilder fullString = new StringBuilder();
+
+      for(String str:board)
+      {
+         fullString.append(str);
+      }
+
+      char[] stringChars = fullString.toString().toCharArray();
+
+      int xCount = 0;
+      int oCount = 0;
+
+      for(char c:stringChars)
+      {
+          if(c == 'X')
+          {
+             xCount++;
+          }else
+          if(c == 'O')
+          {
+            oCount++;
+          }
+
+      }
+
+      if(oCount > xCount || oCount < xCount-1)
+      {
+          return false;
+      }
 
       return true;
    }
 
 
+   /*public static void main(String[] args)
+   {
+      System.out.println("Hello ValidTicTacToe");
+
+      ValidTicTacToe validTicTacToe = new ValidTicTacToe();
+
+      String[] board = {"XOX","O O","XOX"};
+
+      System.out.println("Should be true: " + validTicTacToe.validTicTacToe(board));
+   }*/
+
    public static void main(String[] args)
    {
       System.out.println("Hello ValidTicTacToe");
+
+      ValidTicTacToe validTicTacToe = new ValidTicTacToe();
+
+      String[] board = {"XOX"," X ","   "};
+
+      System.out.println("Should be false: " + validTicTacToe.validTicTacToe(board));
    }
+
 
 }
