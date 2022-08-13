cfadf3b7dad568b66ba1695e4ccb3144587bfdb9
Satish Srinivasan <satsrinister@gmail.com>
2022-08-13 09:16:14 -0700

    Initial commit

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/ValidTicTacToe.java
@@ -0,0 +1,32 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>ValidTicTacToe.java</h1>
+ * This class implements ValidTicTacToe solution.
+ * <p> ValidTicTacToe qn can be found in https://leetcode.com/problems/valid-tic-tac-toe-state/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-13
+ */
+
+
+public class ValidTicTacToe
+{
+
+   public ValidTicTacToe(){}
+
+   public boolean validTicTacToe()
+   {
+
+      return true;
+   }
+
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello ValidTicTacToe");
+   }
+
+}
