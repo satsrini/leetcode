3722b58f9ba3085ea64f8b3389d50ef534710b7c
Satish Srinivasan <satsrinister@gmail.com>
2022-08-14 00:46:28 -0700

    Init

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/TicTacToeWinner.java
@@ -0,0 +1,31 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>TicTacToeWinner.java</h1>
+ * This class implements TicTacToeWinner solution.
+ * <p> TicTacToeWinner qn can be found in https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-14
+ */
+
+public class TicTacToeWinner
+{
+   public TicTacToeWinner(){}
+
+   public String tictactoe(int[][] moves)
+   {
+
+      return "Pending";
+   }
+
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello TicTacToeWinner");
+   }
+
+
+}
