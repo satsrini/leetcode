d35ded7fec250356d5a135020e1c6bdd3d66bb44
Satish Srinivasan <satsrinister@gmail.com>
2022-08-14 01:20:39 -0700

    Init

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/AddStrings.java
@@ -0,0 +1,20 @@
+package com.myleetcode.interviewprep;
+
+
+public class AddStrings
+{
+   public AddStrings(){}
+
+   public String addStrings(String num1, String num2)
+   {
+
+     return null;
+   }
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello AddStrings");
+   }
+
+
+}
