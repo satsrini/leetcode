b2c5e2d3d7b12ba7575bf02a49154073881ffb03
Satish Srinivasan <satsrinister@gmail.com>
2022-08-29 09:20:26 -0700

    Init

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/StrStr.java
@@ -0,0 +1,31 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>StrStr.java</h1>
+ * This class implements StrStr solution.
+ * <p> StrStr qn can be found in https://leetcode.com/problems/implement-strstr/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-29
+ */
+
+public class StrStr
+{
+    public StrStr(){}
+
+    public int strStr(String s)
+    {
+
+
+       return -1;
+    }
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello StrStr");
+    }
+
+}
