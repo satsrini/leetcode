cb8fc752526cfd40468aa094ecd5170669f7d147
Satish Srinivasan <satsrinister@gmail.com>
2022-08-29 07:34:13 -0700

    Init

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/ValidParenthesis.java
@@ -0,0 +1,23 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>ValidParenthesis.java</h1>
+ * This class implements ValidParenthesis solution.
+ * <p> ValidParenthesis qn can be found in https://leetcode.com/problems/valid-parentheses/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-29
+ */
+
+public class ValidParenthesis
+{
+   public ValidParenthesis(){}
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello ValidParenthesis");
+   }
+
+}
