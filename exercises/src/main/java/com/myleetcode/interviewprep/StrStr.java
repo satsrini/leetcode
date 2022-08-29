a2fbc01b988183c87520b3dc4f8496525c27afef
Satish Srinivasan <satsrinister@gmail.com>
2022-08-29 09:35:58 -0700

    done

--- exercises/src/main/java/com/myleetcode/interviewprep/StrStr.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/StrStr.java
@@ -15,17 +15,48 @@ public class StrStr
 {
     public StrStr(){}
 
-    public int strStr(String s)
+    public int strStr(String haystack, String needle)
     {
 
+       if(needle.length() > haystack.length())
+       {
+           return -1;
+       }
 
-       return -1;
+       if(needle.equals(haystack))
+       {
+           return 0;
+       }
+
+       int resultIndex = -1;
+       int count = 0;
+       while( count < haystack.length()-needle.length())
+       {
+
+          if(haystack.substring(count, count+needle.length()).equals(needle))
+          {
+              resultIndex = count;
+              break;
+          }
+          
+          count++;
+
+       }
+
+       return resultIndex;
     }
 
 
     public static void main(String[] args)
     {
         System.out.println("Hello StrStr");
+
+        StrStr strStr = new StrStr();
+        String haystack = "hello";
+        String needle = "ll";
+
+        System.out.println("Should be 2:   " + strStr.strStr(haystack, needle));
+
     }
 
 }
