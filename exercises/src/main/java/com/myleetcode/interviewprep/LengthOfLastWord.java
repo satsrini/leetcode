82b8bfde633cc1d7c6f13dcd023e53853fab618d
Satish Srinivasan <satsrinister@gmail.com>
2022-08-29 09:51:52 -0700

    Init

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/LengthOfLastWord.java
@@ -0,0 +1,25 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>LengthOfLastWord.java</h1>
+ * This class implements LengthOfLastWord solution.
+ * <p> LengthOfLastWord qn can be found in https://leetcode.com/problems/length-of-last-word/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-29
+ */
+
+public class LengthOfLastWord
+{
+
+  public LengthOfLastWord(){}
+
+
+  public static void main(String[] args)
+  {
+     System.out.println("Hello LengthOfLastWord");
+  }
+
+}
