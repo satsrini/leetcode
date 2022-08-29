19c6393a7e801b98ca873cbbf008c5df7d142860
Satish Srinivasan <satsrinister@gmail.com>
2022-08-29 07:58:32 -0700

    all done

--- exercises/src/main/java/com/myleetcode/interviewprep/ValidParenthesis.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/ValidParenthesis.java
@@ -1,5 +1,9 @@
 package com.myleetcode.interviewprep;
 
+import java.util.Stack;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * <h1>ValidParenthesis.java</h1>
  * This class implements ValidParenthesis solution.
@@ -13,11 +17,70 @@ package com.myleetcode.interviewprep;
 
 public class ValidParenthesis
 {
+
+   private static Map<Character,Character> values = new HashMap<>();
+
+   static
+   {
+      values.put('(',')');
+      values.put('{','}');
+      values.put('[',']');
+   }
+
    public ValidParenthesis(){}
+
+   public boolean isValid(String s)
+   {
+
+      if(s.length() == 1)
+      {
+         return false;
+      }
+
+      Stack<Character> stack = new Stack<>();
+
+      char parenthesis;
+
+      for(int i = 0; i < s.length(); i++)
+      {
+         parenthesis = s.charAt(i);
+         if(parenthesis  == '(' || parenthesis  == '{' || parenthesis  == '[')
+         {
+            stack.push(parenthesis);
+            continue;
+         }
+
+         if(values.get(stack.pop()) != parenthesis)
+         {
+            return false;
+         }
+
+      }
+
+      return true;
+   }
+
+   /*public static void main(String[] args)
+   {
+      System.out.println("Hello ValidParenthesis");
+
+      String s = "()[]{}";
+      ValidParenthesis validParenthesis = new ValidParenthesis();
+
+     System.out.println("is Valid: true:  " + validParenthesis.isValid(s));
+
+   }*/
 
    public static void main(String[] args)
    {
       System.out.println("Hello ValidParenthesis");
+
+      String s = "(]";
+      ValidParenthesis validParenthesis = new ValidParenthesis();
+
+      System.out.println("not Valid:false  " + validParenthesis.isValid(s));
+
    }
+
 
 }
