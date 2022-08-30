a3adfb382320ad3f334627add8bade5627dcf66c
Satish Srinivasan <satsrinister@gmail.com>
2022-08-29 22:09:00 -0700

    Init

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/AddBinary.java
@@ -0,0 +1,28 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>AddBinary.java</h1>
+ * This class implements AddBinary solution.
+ * <p> AddBinary qn can be found in https://leetcode.com/problems/add-binary/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-30
+ */
+public class AddBinary
+{
+
+   public AddBinary(){}
+
+   public String addBinary(String a, String b)
+   {
+      return null;
+   }
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello AddBinary");
+   }
+
+}
