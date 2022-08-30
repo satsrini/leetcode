bec13a8d894116c3d547a7274781720c54e42b17
Satish Srinivasan <satsrinister@gmail.com>
2022-08-30 04:12:03 -0700

    Init

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/ValidPalindrome.java
@@ -0,0 +1,30 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>ValidPalindrome.java</h1>
+ * This class implements ValidPalindrome solution.
+ * <p> ValidPalindrome qn can be found in https://leetcode.com/problems/valid-palindrome/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-08-30
+ */
+
+public class ValidPalindrome
+{
+
+    public ValidPalindrome(){}
+
+    public boolean isPalindrome(String s)
+    {
+
+       return true;
+    }
+
+    public static void main(String[] args)
+    {
+       System.out.println("Hello ValidPalindrome");
+    }
+
+}
