fdffbb6e6b991ec5398a29dafd58aca1a9b8562e
Satish Srinivasan <satsrinister@gmail.com>
2022-09-04 09:02:59 -0700

    BEGAN

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/PalindromeNumber.java
@@ -0,0 +1,30 @@
+package com.myleetcode.interviewprep;
+
+/**
+ * <h1>PalindromeNumber.java</h1>
+ * This class implements PalindromeNumber solution.
+ * <p> PalindromeNumber qn can be found in https://leetcode.com/problems/palindrome-number/
+ *
+ *
+ * @author  Satish Srinivasan
+ * @version 1.0
+ * @since   2022-09-04
+ */
+
+public class PalindromeNumber
+{
+
+    public PalindromeNumber(){}
+
+    public boolean isPalindrome(int x)
+    {
+       return true;
+    }
+
+
+    public static void main(String[] args)
+    {
+         System.out.println("Hello Palindrome Number");
+    }
+
+}
