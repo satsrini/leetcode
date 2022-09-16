97247074c6693f339c903cad0beb991283ef4545
Satish Srinivasan <satsrinister@gmail.com>
2022-09-15 23:54:17 -0700

    BASE62 BEGAN

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/Base62.java
@@ -0,0 +1,27 @@
+package com.myleetcode.interviewprep;
+
+public class Base62
+{
+
+
+   private static String BASE_62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+   public Base62(){}
+
+   public String encode(long number)
+   {
+      return null;
+   }
+
+   public long decode(String base64Number)
+   {
+      return 0l;
+   }
+  
+
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello BASE62");
+   }
+
+}
