be3d1e4ad9af9ac0d8cc7beabf19feb1d585a8aa
Satish Srinivasan <satsrinister@gmail.com>
2022-09-17 09:19:20 -0700

    PrintInOrder DONEEE

--- exercises/src/main/java/com/myleetcode/interviewprep/multithread/PrintInOrder.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/multithread/PrintInOrder.java
@@ -3,29 +3,98 @@ package com.myleetcode.interviewprep.multithread;
 public class PrintInOrder
 {
 
+   private Object lock = new Object();
+   private int counter = 1;
+
    public PrintInOrder()
    {
 
    }
 
-   public void first(Runnable printFirst) throws InterruptedException
+   public void first()
    {
-      printFirst.run();
+      synchronized(lock)
+      {
+         System.out.println("First");
+         counter = 2;
+         lock.notifyAll();
+      }
    }
 
-   public void second(Runnable printSecond) throws InterruptedException
+   public void second()
    {
-      printSecond.run();
+
+      synchronized(lock)
+      {
+         while(counter != 2)
+         {
+            try
+            {
+              lock.wait();
+            }catch(InterruptedException e)
+            {
+              e.printStackTrace();
+            }
+         }
+
+         System.out.println("Second");
+         counter = 3;
+         lock.notifyAll();
+      }
    }
 
-   public void third(Runnable printThird) throws InterruptedException
+   public void third()
    {
-      printThird.run();
+
+      synchronized(lock)
+      {
+         while(counter != 3)
+         {
+            try
+            {
+               lock.wait();
+            }catch(InterruptedException e)
+            {
+               e.printStackTrace();
+            }
+         }
+         System.out.println("Third");
+      } 
+
    }
 
    public static void main(String[] args)
    {
       System.out.println("Hello Print In Order");
+
+      PrintInOrder printInOrder = new PrintInOrder();
+
+      Thread t1 = new Thread(() -> printInOrder.first(), "First Thread");
+
+      Thread t2 = new Thread(() -> printInOrder.second(), "Second Thread");
+
+      Thread t3 = new Thread(() -> printInOrder.third(), "Third Thread");
+
+      try
+      {
+        t3.start();
+        t2.start();
+        t1.start();
+      }catch(Exception e)
+      {
+         e.printStackTrace();
+      }
+
+      try
+      {
+        t1.join();
+        t2.join();
+        t3.join();
+      }catch(InterruptedException e)
+      {
+         e.printStackTrace();
+      }
+
    }
 
 }
