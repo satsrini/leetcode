451dbf1231de57624f8abf59d92f3da2dd418753
Satish Srinivasan <satsrinister@gmail.com>
2022-09-18 01:52:36 -0700

    BNEGANNN

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/multithread/TrafficLight.java
@@ -0,0 +1,26 @@
+package com.myleetcode.interviewprep.multithread;
+
+
+public class TrafficLight
+{
+
+    public TrafficLight()
+    {
+
+    }
+
+    public void carArrived(int carId,
+                           int roadId,
+                           int direction,
+                           Runnable turnGreen,
+                           Runnable crossCar)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Traffic Light");
+    }
+
+}
