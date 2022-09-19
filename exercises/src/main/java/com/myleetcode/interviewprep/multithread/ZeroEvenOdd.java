a48a9b40dc23c538926c4fe5af766adeb2412f4f
Satish Srinivasan <satsrinister@gmail.com>
2022-09-19 01:48:18 -0700

    Zewro Even Odd BEGAN

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/multithread/ZeroEvenOdd.java
@@ -0,0 +1,35 @@
+package com.myleetcode.interviewprep.multithread;
+
+import java.util.function.IntConsumer;
+
+public class ZeroEvenOdd
+{
+
+   private int n;
+
+   public ZeroEvenOdd(int n)
+   {
+
+   }
+
+   public void zero(IntConsumer printConsumer) throws InterruptedException
+   {
+      
+   }
+
+   public void even(IntConsumer printConsumer) throws InterruptedException
+   {
+
+   }
+
+   public void odd(IntConsumer printConsumer) throws InterruptedException
+   {
+
+   }
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello ZeroEvenOdd");
+   }
+
+}
