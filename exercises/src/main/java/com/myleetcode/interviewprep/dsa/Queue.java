2eebdaa11ae72ad7757282461fb61084cabb0a93
Satish Srinivasan <satsrinister@gmail.com>
2022-09-20 08:51:36 -0700

    Queue BEGAN

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/dsa/Queue.java
@@ -0,0 +1,55 @@
+package com.myleetcode.interviewprep.dsa;
+
+import java.util.Iterator;
+
+public class Queue<T> implements Iterable<T>
+{
+
+   public Queue()
+   {
+
+   }
+
+   public void enqueue(T t)
+   {
+
+   }
+
+   public T dequeue()
+   {
+      return null;
+   }
+
+   public boolean isEmpty()
+   {
+      return true;
+   }
+
+   public int size()
+   {
+      return 0;
+   }
+
+   @Override
+   public Iterator<T> iterator()
+   {
+     return null;
+   }
+
+   class QueueIterator<T> implements Iterator<T>
+   {
+
+      @Override
+      public boolean hasNext()
+      {
+          return false;
+      }
+
+      @Override
+      public T next()
+      {
+          return null;
+      }
+
+   }
+}
