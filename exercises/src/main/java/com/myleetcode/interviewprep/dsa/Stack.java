36d586db5676746ddea19275dc62589e6b1cf6ff
Satish Srinivasan <satsrinister@gmail.com>
2022-09-20 11:12:38 -0700

    More Test cases for Stack

--- exercises/src/main/java/com/myleetcode/interviewprep/dsa/Stack.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/dsa/Stack.java
@@ -79,7 +79,7 @@ public class Stack<T> implements Iterable<T>
 
    }
 
-   public static void main(String[] args)
+   /*public static void main(String[] args)
    {
       Stack<Integer> stack = new Stack<>();
 
@@ -131,6 +131,81 @@ public class Stack<T> implements Iterable<T>
       System.out.println("\n\n");
 
 
-   }
+   }*/
+
+   public static void main(String[] args)
+   {
+      Stack<Integer> stack = new Stack<>();
+
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
+      stack.push(4);
+      stack.push(5);
+
+      System.out.println("SIZE: " + stack.size());
+
+      System.out.println("\n\n");
+
+      for(int i:stack)
+      {
+         System.out.print(" " + i);
+      }
+
+      System.out.println("\n\n");
+
+      System.out.println("\n\n");
+
+      for(int i:stack)
+      {
+         System.out.print(" " + i);
+      }
+
+      System.out.println("\n\n");
+
+      System.out.println("\n\n");
+      System.out.println("SIZE: " + stack.size());
+
+      System.out.println("POPPING");
+      System.out.println(stack.pop());
+      System.out.println(stack.pop());
+      System.out.println("SIZE: " + stack.size());
+
+      System.out.println("\n\n");
+
+      System.out.println("\n\n");
+
+      for(int i:stack)
+      {
+         System.out.print(" " + i);
+      }
+
+      System.out.println("\n\n");
+
+      System.out.println("\n\n");
+
+      System.out.println("SIZE: " + stack.size());
+
+      System.out.println("PUSHONG");
+      stack.push(6);
+      stack.push(7);
+      System.out.println("SIZE: " + stack.size());
+
+      System.out.println("\n\n");
+
+      System.out.println("\n\n");
+
+      for(int i:stack)
+      {
+         System.out.print(" " + i);
+      }
+
+      System.out.println("\n\n");
+
+      System.out.println("\n\n");
+
+
+
+    }
 
 }
