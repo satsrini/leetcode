5cdc82508198911f3b45214443718470fdec5cc0
Satish Srinivasan <satsrinister@gmail.com>
2022-09-20 10:44:49 -0700

    QUEUE DONE ???

--- exercises/src/main/java/com/myleetcode/interviewprep/dsa/Queue.java
+++ exercises/src/main/java/com/myleetcode/interviewprep/dsa/Queue.java
@@ -5,50 +5,90 @@ import java.util.Iterator;
 public class Queue<T> implements Iterable<T>
 {
 
+   private Node<T> head;
+   private Node<T> current = head;
+   private int N;
+
    public Queue()
    {
-
+      N = 0;
    }
 
    public void enqueue(T t)
    {
+      if(isEmpty())
+      {
+         head = new Node<>(t, null);
+         current = head;
+         return;
+      }
+
+      Node<T> x = new Node<>(t,null);
+      current.next = x;
+      current = current.next;
+      N++;
 
    }
 
    public T dequeue()
    {
-      return null;
+
+      if(isEmpty())
+      {
+         return null;
+      }
+
+      Node<T> x = head;
+      head = head.next;
+      N--;
+
+      return x.value;
    }
 
    public boolean isEmpty()
    {
-      return true;
+      return N==0;
    }
 
    public int size()
    {
-      return 0;
+      return N;
    }
 
    @Override
    public Iterator<T> iterator()
    {
-     return null;
+     return new QueueIterator<>(this);
    }
 
    class QueueIterator<T> implements Iterator<T>
    {
 
+      private Node<T> currentNode;
+
+      QueueIterator(Queue<T> queue)
+      {
+         currentNode = queue.head;
+      }
+
       @Override
       public boolean hasNext()
       {
-          return false;
+          return currentNode != null;
       }
 
       @Override
       public T next()
       {
-          return null;
+
+          if(currentNode == null)
+          {
+              return null;
+          }
+
+          Node<T> x = currentNode;
+          currentNode = currentNode.next;
+          return x.value;
       }
 
    }
