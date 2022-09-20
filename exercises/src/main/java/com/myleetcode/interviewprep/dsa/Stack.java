73c6f564d78ac9e6dabe11b9fea88b381b972033
Satish Srinivasan <satsrinister@gmail.com>
2022-09-20 07:50:49 -0700

    STACK BEGAN

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/dsa/Stack.java
@@ -0,0 +1,38 @@
+package com.myleetcode.interviewprep.dsa;
+
+import java.util.Iterator;
+
+public class Stack<T> implements Iterable<T>
+{
+   public Stack()
+   {
+
+   }
+
+   public void push(T t)
+   {
+
+   }
+
+   public T pop()
+   {
+     return null;
+   }
+
+   public boolean isEmpty()
+   {
+      return true;
+   }
+
+   public int size()
+   {
+      return 0;
+   }
+
+   @Override
+   public Iterator<T> iterator()
+   {
+      return null;
+   }
+
+}
