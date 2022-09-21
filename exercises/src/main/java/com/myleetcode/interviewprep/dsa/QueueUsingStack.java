ae4d33057cb1564bf06ec08bd155e16766bc12b1
Satish Srinivasan <satsrinister@gmail.com>
2022-09-21 09:50:12 -0700

    BEGAN

---
+++ exercises/src/main/java/com/myleetcode/interviewprep/dsa/QueueUsingStack.java
@@ -0,0 +1,34 @@
+package com.myleetcode.interviewprep.dsa;
+
+/*
+ *
+ * Leetcode: https://leetcode.com/problems/implement-queue-using-stacks/
+ *
+ */
+
+public class QueueUsingStack<T>
+{
+
+    public QueueUsingStack(){}
+
+    public void enqueue(T t)
+    {
+
+    }
+
+    public T dequeue()
+    {
+       return null;
+    }
+
+    public T peek()
+    {
+      return null;
+    }
+
+    public boolean isEmpty()
+    {
+      return true;
+    }
+
+}
